679846964dedab1b5d5ad001e226b5399702cb14
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2024-10-06 22:42:58 +0300

    updated the project setup
    added fonts and assets

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
